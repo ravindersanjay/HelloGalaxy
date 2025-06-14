9124d6d18304f868ebc28884d62daab6c01744d0
jitu <jitu@bhai.com>
2025-06-14 22:07:40 +0530

    first commit

---
+++ src/test/java/LoginPage.java
@@ -0,0 +1,34 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class LoginPage {
+
+	@Test
+	public void Login()
+	{
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--start-maximized");
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+		driver.get("https://demoqa.com/login");
+		driver.findElement(By.id("userName")).sendKeys("ravinderKumar");
+		driver.findElement(By.id("password")).sendKeys("Password@123456");
+		driver.findElement(By.id("login")).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("submit")));
+		
+		driver.quit();
+	}
+	
+	
+}
